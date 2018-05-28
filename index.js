ae49e117358b487eaaa6f91e01e187894f7d74a6
Liane Chan <liane.chan@gmail.com>
2018-05-28 18:38:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 var recipes = { foo: 'bar' }
 
 function updateObjectWithKeyAndValue(object, key, value) {
-   return object.assign({}, object, { [key]: value })
+   return object.assign({}, object, { [key]: value });
 }
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value
+  object[key] = value;
 }
 
 function  deleteFromObjectByKey(object, key) {
   delete object.key; // true
-object;
+ return object;
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
