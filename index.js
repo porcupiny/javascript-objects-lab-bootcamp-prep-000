8dd73bb31cd1205e7b0f76265b1f838cdc647cd9
Liane Chan <liane.chan@gmail.com>
2018-05-28 18:26:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function  updateObjectWithKeyAndValue(object, key, value) {
   
 }
 
-function  updateObjectWithKeyAndValue(object, key, value) {
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   
 }
